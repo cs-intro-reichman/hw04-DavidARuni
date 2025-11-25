b0082c4b1fa0b51e0898d4aaf6421133e6e1189d
David <david.20797@gmail.com>
2025-11-25 10:34:48 +0200

    Primes - working faster

--- Primes.java
+++ Primes.java
@@ -8,19 +8,19 @@ public class Primes {
         
         //Set a new array with [f,f,t,t,...,t](n+1)
         isPrime = new boolean[n+1];
-        for(int i = 2; i < n+1 ; i++){
+        for(int i = 2; i <= n ; i++){
             isPrime[i] = true;
         }
 
         int start = 2;
-        do {
+        
+        while (start * start <= n){
             
             if ( isPrime[start] ){
                 crossNonPrimes(start);
             }
             start++;
-            
-        } while (start<=isPrime.length);
+        } 
 
         print(n);
     }
@@ -30,22 +30,28 @@ public class Primes {
      * @param num : Base number to cross primes 
      */
     private static void crossNonPrimes(int num){
-        for (int i = num; i < isPrime.length; i++){
-            if(isPrime[i] && i % num == 0){
-                isPrime[i] = false;
-            }
+        // num*num -> the next number for a prime
+        // i += num -> eliminates the need for if % == 0 
+        for (int i = num*num; i < isPrime.length; i += num){
+            isPrime[i] = false;
         }
     }
 
-    private static void print(int n){
+    private static void print(int n) {
         int numOfPrimes = 0; 
-        for(int i = 0; i < isPrime.length; i++){
-            if (isPrime[i]){
+
+        System.out.println("Prime numbers up to " + n + ":");
+        for (int i = 2; i < isPrime.length; i++) {
+            if (isPrime[i]) {
                 numOfPrimes++;
                 System.out.println(i);
             }
         }
-        int per = (int)(100 * numOfPrimes / n );
-        System.err.println("There are " + numOfPrimes + " primes between 2 and " + n + ". (" + per + "% are primes)");
+
+        int per = (int)(100 * numOfPrimes / n);
+        System.out.println("There are " + numOfPrimes + 
+                        " primes between 2 and " + n + 
+                        " (" + per + "% are primes)");
+
     }
 }
